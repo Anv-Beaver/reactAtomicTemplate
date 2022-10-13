774e98000b17fdfcd9f7f3838314f46ec89f79a2
JaehyeongWang <jahyeng224@g.skku.edu>
2022-10-13 21:10:16 +0900

    1013

---
+++ src/pages/Example/atoms/Logo.tsx
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+
+export const Logobox = styled.div`
+    position: relative;
+    top: 6px;
+    left: 34px;
+    height: 85.5px;
+    align-self: stretch;
+    flex-grow: 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: stretch;
+    padding: 0 20px;
+        
+`
+
+export const ANV_1 = styled.div`
+    width: 209.9px;
+    height: 85.2px;
+    position: absolute;
+    left: 26.7px;
+    font-family: Archivo;
+    font-size: 55px;
+    font-weight: 600;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: 0.65;
+    letter-spacing: 0.1px;
+    text-align: left;
+    color: #1e293b;
+    line-height: 85.2px;   
+`
+
+export const Rectangle_1 = styled.div`
+    position: absolute;
+    top: 40px;
+    left: 41px;
+    width: 189px;
+    height: 33px;
+    transform: rotate(-359.67deg);
+    background-color: #77a7f5;
+`
---
+++ src/pages/Example/modules/header.tsx
@@ -0,0 +1,15 @@
+import { Logobox, ANV_1, Rectangle_1 } from "../atoms/Logo";
+
+export const Logo = () => {
+    return (
+        <div>
+            <Logobox>
+                <Rectangle_1 />
+                <ANV_1>  ANV_1 
+                </ANV_1> 
+            </Logobox>
+            
+        </div>
+        
+    );
+}
