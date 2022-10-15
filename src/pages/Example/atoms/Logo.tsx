d9aafd611ff9471d2d84f73cb42fc8a5b77c0eb5
JaehyeongWang <jahyeng224@g.skku.edu>
2022-10-15 23:15:37 +0900

    add:logo

--- src/pages/Example/atoms/Logo.tsx
+++ src/pages/Example/atoms/Logo.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 
+
 export const Logobox = styled.div`
     position: relative;
     top: 6px;
--- src/pages/Example/modules/header.tsx
+++ src/pages/Example/modules/header.tsx
@@ -1,4 +1,6 @@
-import { Logobox, ANV_1, Rectangle_1 } from "../atoms/Logo";
+import { Logobox, ANV_1, Rectangle_1} from "../atoms/Logo";
+import ic24_menu from './ic24_menu.png';
+
 
 export const Logo = () => {
     return (
@@ -8,7 +10,7 @@ export const Logo = () => {
                 <ANV_1>  ANV_1 
                 </ANV_1> 
             </Logobox>
-            
+            <img src={ic24_menu}></img>
         </div>
         
     );
